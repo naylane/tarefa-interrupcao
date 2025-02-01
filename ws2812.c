f78e7f5bb15bc5368140f6806931b596f15d222b
Naylane Ribeiro <naylaneribeiro4@gmail.com>
2025-02-01 07:32:10 -0300

    style: organizando código

--- ws2812.c
+++ ws2812.c
@@ -1,16 +1,3 @@
-/*
- * Por: Wilton Lacerda Silva
- * 
- * Este código é uma adaptação do código original do SDK Pico
- * para a utilização da matriz de LEDs WS2812 do BitDogLab.
- * 
- * A seleção de LEDs acesos é feita por meio de um buffer de LEDs, onde
- * cada posição do buffer representa um LED da matriz 5x5.
- * 
- * Original em:
- * https://github.com/raspberrypi/pico-examples/tree/master/pio/ws2812
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include "pico/stdlib.h"
@@ -30,7 +17,7 @@
 static volatile uint current_number = 0;
 static volatile uint32_t last_time = 0; // Armazena o tempo do último evento (em microssegundos)
 
-// Buffer para armazenar quais LEDs estão ligados matriz 5x5
+// Buffer do padrão de desenhos para a matriz 5x5
 bool led_buffer[10][25] = {
     0, 1, 1, 1, 0, 
     1, 0, 0, 0, 1, 
@@ -57,55 +44,18 @@ bool led_buffer[10][25] = {
     1, 1, 1, 1, 0
 };
 
-// Prototipação da função de interrupção
+// Prototipo de funções
 static inline void put_pixel(uint32_t pixel_grb);
 static inline uint32_t urgb_u32(uint8_t r, uint8_t g, uint8_t b);
 void set_led(uint8_t r, uint8_t g, uint8_t b);
+
+/**
+ * @brief Função de interrupção com debouncing.
+ * 
+ * @param gpio a GPIO que gerou interrupção.
+ * @param events a evento que gerou interrupção.
+ */
 static void gpio_irq_handler(uint gpio, uint32_t events);
-
-
-static inline void put_pixel(uint32_t pixel_grb) {
-    pio_sm_put_blocking(pio0, 0, pixel_grb << 8u);
-}
-
-static inline uint32_t urgb_u32(uint8_t r, uint8_t g, uint8_t b) {
-    return ((uint32_t)(r) << 8) | ((uint32_t)(g) << 16) | (uint32_t)(b);
-}
-
-void set_led(uint8_t r, uint8_t g, uint8_t b) {
-    // Define a cor com base nos parâmetros fornecidos
-    uint32_t color = urgb_u32(r, g, b);
-
-    // Define todos os LEDs com a cor especificada
-    for (int i = 0; i < 25; i++) {
-        if (led_buffer[current_number][i]) {
-            put_pixel(color); // Liga o LED com um no buffer
-        }
-        else {
-            put_pixel(0);  // Desliga os LEDs com zero no buffer
-        }
-    }
-}
-
-// Função de interrupção com debouncing
-static void gpio_irq_handler(uint gpio, uint32_t events) {
-    // Obtém o tempo atual em microssegundos
-    uint32_t current_time = to_us_since_boot(get_absolute_time());
-    printf("A = %d\n", current_number);
-    // Verifica se passou tempo suficiente desde o último evento
-    if (current_time - last_time > 2000000) // 200 ms? de debouncing
-    {
-        last_time = current_time; // Atualiza o tempo do último evento
-
-        if (gpio == 5) {
-            set_led(255, 102, 178); // setando os leds na cor rosa
-            if(current_number < 10) { current_number++; }
-        } else if (gpio == 6) {
-            set_led(255, 102, 178); // setando os leds na cor rosa
-            if(current_number > 0) { current_number--; }
-        }
-    }
-}
 
 
 int main() {
@@ -135,8 +85,7 @@ int main() {
     gpio_set_irq_enabled_with_callback(A_BTN_PIN, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
     gpio_set_irq_enabled_with_callback(B_BTN_PIN, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
 
-    while (1) 
-    {
+    while (1) {
         gpio_put(RED_LED_PIN, 1);
         sleep_ms(1000);
         gpio_put(RED_LED_PIN, 0);
@@ -144,4 +93,45 @@ int main() {
     }
 
     return 0;
+}
+
+static inline void put_pixel(uint32_t pixel_grb) {
+    pio_sm_put_blocking(pio0, 0, pixel_grb << 8u);
+}
+
+static inline uint32_t urgb_u32(uint8_t r, uint8_t g, uint8_t b) {
+    return ((uint32_t)(r) << 8) | ((uint32_t)(g) << 16) | (uint32_t)(b);
+}
+
+void set_led(uint8_t r, uint8_t g, uint8_t b) {
+    // Define a cor com base nos parâmetros fornecidos
+    uint32_t color = urgb_u32(r, g, b);
+
+    // Define todos os LEDs com a cor especificada
+    for (int i = 0; i < 25; i++) {
+        if (led_buffer[current_number][i]) {
+            put_pixel(color); // Liga o LED com um no buffer
+        }
+        else {
+            put_pixel(0);  // Desliga os LEDs com zero no buffer
+        }
+    }
+}
+
+static void gpio_irq_handler(uint gpio, uint32_t events) {
+    // Obtém o tempo atual em microssegundos
+    uint32_t current_time = to_us_since_boot(get_absolute_time());
+    printf("A = %d\n", current_number);
+    // Verifica se passou tempo suficiente desde o último evento
+    if (current_time - last_time > 2000000) { // 200 ms? de debouncing
+        last_time = current_time; // Atualiza o tempo do último evento
+
+        if (gpio == 5) {
+            set_led(255, 102, 178); // setando os leds na cor rosa
+            if(current_number < 10) { current_number++; }
+        } else if (gpio == 6) {
+            set_led(255, 102, 178); // setando os leds na cor rosa
+            if(current_number > 0) { current_number--; }
+        }
+    }
 }
