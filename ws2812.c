94c9b22313187fed4495ed399bde31b1ac7ce0ee
Naylane Ribeiro <naylaneribeiro4@gmail.com>
2025-02-01 08:44:55 -0300

    fix: bug no debouncing

--- ws2812.c
+++ ws2812.c
@@ -10,7 +10,6 @@
 #define RED_LED_PIN 13
 #define A_BTN_PIN 5
 #define B_BTN_PIN 6
-#define IS_RGBW false
 #define WS2812_PIN 7
 
 // Variáveis globais
@@ -20,28 +19,64 @@ static volatile uint32_t last_time = 0; // Armazena o tempo do último evento (e
 // Buffer do padrão de desenhos para a matriz 5x5
 bool led_buffer[10][25] = {
     0, 1, 1, 1, 0, 
-    1, 0, 0, 0, 1, 
-    1, 0, 0, 0, 1, 
-    1, 0, 0, 0, 1, 
+    0, 1, 0, 1, 0, 
+    0, 1, 0, 1, 0, 
+    0, 1, 0, 1, 0, 
     0, 1, 1, 1, 0,
 
-    0, 0, 1, 0, 0, 
-    0, 1, 1, 0, 0, 
-    0, 0, 1, 0, 0, 
-    0, 0, 1, 0, 0, 
+    0, 1, 1, 1, 0,
+    0, 0, 1, 0, 0,
+    0, 0, 1, 0, 0,
+    0, 1, 1, 0, 0,
     0, 0, 1, 0, 0,
 
-    0, 1, 1, 1, 0, 
-    1, 0, 0, 1, 0, 
-    0, 0, 1, 0, 0, 
-    0, 1, 0, 0, 0, 
-    1, 1, 1, 1, 1,
+    0, 1, 1, 1, 0,
+    0, 1, 0, 0, 0,
+    0, 0, 1, 0, 0,
+    0, 0, 0, 1, 0,
+    0, 1, 1, 1, 0,
 
-    1, 1, 1, 1, 0, 
-    0, 0, 0, 1, 0, 
-    0, 1, 1, 0, 0, 
-    0, 0, 0, 1, 0, 
-    1, 1, 1, 1, 0
+    0, 1, 1, 1, 0,
+    0, 0, 0, 1, 0,
+    0, 1, 1, 1, 0,
+    0, 0, 0, 1, 0,
+    0, 1, 1, 1, 0,
+
+    0, 1, 0, 0, 0,
+    0, 0, 0, 1, 0,
+    0, 1, 1, 1, 0,
+    0, 1, 0, 1, 0,
+    0, 1, 0, 1, 0,
+
+    0, 1, 1, 1, 0,
+    0, 0, 0, 1, 0,
+    0, 1, 1, 1, 0,
+    0, 1, 0, 0, 0,
+    0, 1, 1, 1, 0,
+
+    0, 1, 1, 1, 0,
+    0, 1, 0, 1, 0,
+    0, 1, 1, 1, 0,
+    0, 1, 0, 0, 0,
+    0, 1, 1, 1, 0,
+
+    0, 0, 0, 1, 0,
+    0, 1, 0, 0, 0,
+    0, 0, 1, 0, 0,
+    0, 0, 0, 1, 0,
+    0, 1, 1, 1, 0,
+
+    0, 1, 1, 1, 0,
+    0, 1, 0, 1, 0,
+    0, 1, 1, 1, 0,
+    0, 1, 0, 1, 0,
+    0, 1, 1, 1, 0,
+
+    0, 1, 1, 1, 0,
+    0, 0, 0, 1, 0,
+    0, 1, 1, 1, 0,
+    0, 1, 0, 1, 0,
+    0, 1, 1, 1, 0
 };
 
 // Prototipo de funções
@@ -56,7 +91,7 @@ void set_led(uint8_t r, uint8_t g, uint8_t b);
  * @param events a evento que gerou interrupção.
  */
 static void gpio_irq_handler(uint gpio, uint32_t events);
-
+void apagar_leds();
 
 int main() {
     // Inicializações
@@ -76,10 +111,11 @@ int main() {
     gpio_pull_up(B_BTN_PIN);
 
     PIO pio = pio0;
-    int sm = 0;
     uint offset = pio_add_program(pio, &ws2812_program);
 
-    ws2812_program_init(pio, sm, offset, WS2812_PIN, 800000, IS_RGBW);
+    ws2812_program_init(pio, 0, offset, WS2812_PIN, 800000, false);
+
+    apagar_leds();
 
     // Configuração das interrupções dos botões com callback
     gpio_set_irq_enabled_with_callback(A_BTN_PIN, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
@@ -121,17 +157,23 @@ void set_led(uint8_t r, uint8_t g, uint8_t b) {
 static void gpio_irq_handler(uint gpio, uint32_t events) {
     // Obtém o tempo atual em microssegundos
     uint32_t current_time = to_us_since_boot(get_absolute_time());
-    printf("A = %d\n", current_number);
+    
     // Verifica se passou tempo suficiente desde o último evento
-    if (current_time - last_time > 2000000) { // 200 ms? de debouncing
+    if (current_time - last_time > 200000) { // 200 ms? de debouncing
         last_time = current_time; // Atualiza o tempo do último evento
 
         if (gpio == 5) {
-            set_led(255, 102, 178); // setando os leds na cor rosa
             if(current_number < 10) { current_number++; }
+            set_led(22, 1, 12); // setando os leds na cor rosa
         } else if (gpio == 6) {
-            set_led(255, 102, 178); // setando os leds na cor rosa
             if(current_number > 0) { current_number--; }
+            set_led(22, 1, 12); // setando os leds na cor rosa
         }
     }
+}
+
+void apagar_leds() {
+    for (int i = 0; i < 25; i++) {
+        put_pixel(0);
+    } 
 }
